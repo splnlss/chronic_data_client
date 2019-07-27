223521db0022afb1f33d74396a43159f568431c5
splnlss <jon@splnlss.com>
2019-07-26 23:15:52 -0400

    added server url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://nameless-bastion-74536.herokuapp.com/api' || 'http://localhost:8080/api';
 
 export const AWS_BASE_URL = 'https://chronicdata-records.s3.us-east-2.amazonaws.com/';
