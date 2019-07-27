92b94cbca84a113c989131f8685bd51919f542fb
splnlss <jon@splnlss.com>
2019-07-26 23:19:03 -0400

    remove localhost

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://nameless-bastion-74536.herokuapp.com/api' || 'http://localhost:8080/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://nameless-bastion-74536.herokuapp.com/api'
+
+
 
 export const AWS_BASE_URL = 'https://chronicdata-records.s3.us-east-2.amazonaws.com/';
+
+
+//  || 'http://localhost:8080/api';
