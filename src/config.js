13d6bfe9fb013aab294ff36fe67bb67c7c3fc4ac
splnlss <jon@splnlss.com>
2019-12-11 17:02:01 -0500

    Read backend URL from environment.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const API_BASE_URL =
-'https://nameless-bastion-74536.herokuapp.com/api'; 
+  process.env.REACT_APP_API_BASE_URL ||
+  'http://localhost:8080/api';
+//'https://nameless-bastion-74536.herokuapp.com/api'; 
 //'http://localhost:8080/api';
 
 
